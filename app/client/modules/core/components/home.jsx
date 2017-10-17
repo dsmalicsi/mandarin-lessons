e9050f1194280496efb68f53cb0945b385fcac7b
daryllm <daryllm@sonarlogic.biz>
2017-10-17 14:45:57 +0800

    Initial Layout

--- app/client/modules/core/components/home.jsx
+++ app/client/modules/core/components/home.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import {Link} from 'react-router';
 
 const Home = () => (
   <div>
-    <h1>Hello World</h1>
-    <p>
-      Welcome!
-    </p>
+    <div id="header">
+      <h1 id="page-title">Learn Chinese</h1>
+    </div>
+    <div id="content">
+      <p>
+        Welcome! Please select a lesson:
+      </p>
+
+      <Link className="btn-link" to="fundamentals">FUNDAMENTALS</Link>
+      <Link className="btn-link" to="mandarin">BASIC MANDARIN</Link>
+      <Link className="btn-link" to="tones">TONES</Link>
+      <Link className="btn-link" to="stroke">STROKE</Link>
+
+    </div>
   </div>
 );
 
